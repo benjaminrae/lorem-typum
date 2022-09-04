289358e3d6181e7e2dfcf9f7e474c514a8dcad43
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-09-04 21:32:35 +0200

    Add basic header component

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="header__title">
+                Lorem Typum<span className="header__text-cursor">|</span>
+            </h1>
+        </header>
+    );
+};
+
+export default Header;
